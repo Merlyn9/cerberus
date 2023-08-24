b21861963da799280f66ce9fc7e39a2c44aedaad
Jason Mills <merlyndevelops@gmail.com>
2023-08-23 23:08:41 -0400

    Add files via upload

---
+++ coder1.py
@@ -0,0 +1,23 @@
+# first import the module
+import webbrowser
+  
+# then make a url variable
+url = "https://www.merlyndevelops.com"
+  
+# then call the default open method described above
+webbrowser.open(url)
+
+url1 = "https://code.visualstudio.com/download"
+url2 = "https://www.raspberrypi.com/software/"
+url3 = "https://nmap.org/download"
+url4 = "https://www.python.org/downloads/"
+url5 = "https://www.kali.org/get-kali/#kali-installer-images"
+url6 = "https://www.virtualbox.org/wiki/Downloads"
+
+
+webbrowser.open(url1)
+webbrowser.open(url2)
+webbrowser.open(url3)
+webbrowser.open(url4)
+webbrowser.open(url5)
+webbrowser.open(url6)
